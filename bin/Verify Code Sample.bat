30ca2219c9549da455badd691e476f6e8b570053
Tristan Damron <tdamron@esri.com>
2020-06-20 11:32:22 -0700

    Program verifies that Python and Pylint are installed before running

--- bin/Verify Code Sample.bat
+++ bin/Verify Code Sample.bat
@@ -1,3 +1,14 @@
 @echo off
+python -V | find /v "Python" >NUL 2>NUL && (goto :NOPYTHON)
+python -V | find "Python" >NUL 2>NUL && (goto :PYTHON)
+
+:NOPYTHON
+echo "Error: No installation of Python detected. Please install Python 3.x before continuing."
+start "" "https://python.org/downloads/windows"
+
+:PYTHON
+:: @todo(tdamron): Rather than install Pylint every time the program is run, it is better to check if it's installed once.
+python -m pip install pylint 
 python "../src/verify.py" %1
+
 @pause
